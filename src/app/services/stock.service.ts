2a9c159c0b1c6294d67b5944a86ff62e18c0dc5d
toshendra <toshendra@gmail.com>
2021-01-04 09:16:34 +0000

    Stock service change

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -10,7 +10,8 @@ constructor() {
 this.stocks = [
 new Stock('Test Stock Company', 'TSC', 85, 80, 'NASDAQ'),
 new Stock('Second Stock Company', 'SSC', 10, 20, 'NSE'),
-new Stock('Last Stock Company', 'LSC', 876, 765, 'NYSE')
+new Stock('Last Stock Company', 'LSC', 876, 765, 'NYSE'),
+new Stock('Real Stock Company', 'RSC', 131, 132, 'LSE')
 ];
 }
 getStocks() : Stock[] {
